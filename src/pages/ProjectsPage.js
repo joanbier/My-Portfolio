b2ce7b935f0d8451eab87eac42e3217a0b280927
Joanna Biernat <joannabiernat@MacBook-Pro-Joanna.local>
2021-08-24 09:36:13 +0200

    lazy-loaded-component removed

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,7 +1,7 @@
 import { ProjectsContainer } from "../components/ProjectsElements";
 import content from "../content/index";
 import { Card } from "../components/Card";
-import { LazyLoadComponent } from "react-lazy-load-image-component";
+// import { LazyLoadComponent } from "react-lazy-load-image-component";
 
 const ProjectsPage = ({ lang }) => {
   const getProjects = content.projects.map((item, index) => (
@@ -13,14 +13,12 @@ const ProjectsPage = ({ lang }) => {
   ));
 
   return (
-    <LazyLoadComponent>
       <ProjectsContainer id="projects">
         <h2 className="section-title">
           {lang === "PL" ? "Moje Projekty" : "My Projects"}
         </h2>
         <div className="cards-wrapper">{getProjects}</div>
       </ProjectsContainer>
-    </LazyLoadComponent>
   );
 };
 
